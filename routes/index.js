3040d4c8b666e6eab7a54916e3c5ef307010aa4d
Travis Shears <btbtravis@gmail.com>
2018-03-25 12:57:59 -0500

    Swap wordpress cms for Cockpit CMS

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+//util functions
+let fullPathImages = function (res) {
+  let imgKeys = [];
+  for(var key in res.data.fields) if(res.data.fields[key].type === 'image') imgKeys.push(key);
+  res.data.entries = res.data.entries.map(entry => {
+    imgKeys.map(key => {
+      //api/cockpit/image?token=xxtokenxx&src=path&w=200&h=200&o=true
+      if(entry.hasOwnProperty(key)) entry[key] = (process.env.CMS_PATH.substring(0, process.env.CMS_PATH.length - 4)) + entry[key].path;
+    });
+    return entry;
+  });
+  return res;
+}
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var getCMSData = axios.get(process.env.CMS_PATH + '?rest_route=/wp/v2/sections&_embed')
+  var getCMSData = axios.get(process.env.CMS_PATH + 'collections/get/home_page_sections?token=' + process.env.CMS_TOKEN)
     .then(function (cmsData) {
-      // console.log({cmsData: cmsData.data});
-      cmsData.data.reverse();
-      let h1s = cmsData.data.reduce(function (arr, section) {
-        var titles = [];
-        var regex1 = RegExp('h1 id="([^"]+)">([^<]+)','g');
-        var matches;
-        while ((matches = regex1.exec(section.body_content)) !== null) {
-          titles.push([matches[1], matches[2]]);
-        }
-        return arr.concat(titles);
-      }, [
-        ['volunteer_opportunities', 'Volunteer Opportunities'],
-        ['upcoming_events', 'Upcoming Events']
-      ]);
-      let sections =cmsData.data.map(function (section) {
-        section.featuredImage = section._embedded['wp:featuredmedia'][0].source_url;
-        return section;
-      });
-      return { sections: sections, titles: h1s };
+      //Titles
+      cmsData = fullPathImages(cmsData);
+      let staticTitles = [
+        ['upcoming_events', 'Upcoming Events'],
+        ['volunteer_opportunities', 'Volunteer Opportunities']
+      ];
+      let titles = cmsData.data.entries.map((entry, i) => ["section_" + i, entry.title]);
+      let finalTitles = staticTitles.concat(titles);
+      return { sections: cmsData.data.entries, titles: finalTitles  };
     });
   var getEvents = new Promise((resolve, reject) => {
     req.eventdb.find({ event_type: 'signupgenius' }).sort({ starttime: 1 }).exec(function (err, docs) {
@@ -40,17 +43,19 @@ router.get('/', function (req, res, next) {
       resolve(docs);
     });
   });
-  var getCallsToAction = axios.get(process.env.CMS_PATH + '?rest_route=/wp/v2/call-to-action')
+  var getCallsToAction = Promise.resolve([]);
+    //axios.get(process.env.CMS_PATH + '?rest_route=/wp/v2/call-to-action')
+    //.then(function (cmsData) {
+      //return cmsData.data[0].action_text;
+    //});
+
+  var getHomePageCards = axios.get(process.env.CMS_PATH + 'collections/get/home_page_cards?token=' + process.env.CMS_TOKEN)
     .then(function (cmsData) {
-      return cmsData.data[0].action_text;
-    });
-  var getHomePageCards = axios.get(process.env.CMS_PATH + '?rest_route=/wp/v2/home_page_card&_embed')
-    .then(function (cmsData) {
-      return cmsData.data.map(function (card) {
-        card.featuredImage = card._embedded['wp:featuredmedia'][0].source_url;
-        return card;
+      cmsData = fullPathImages(cmsData);
+      console.log("Cards: ", cmsData.data.entries);
+      return cmsData.data.entries;
+      //return fullPathImages(cmsData);
       });
-    });
   Promise.all([getCMSData, getEvents, getFacebookEvents, getFacebookPhotos, getCallsToAction, getHomePageCards]).then(function (results) {
     var data = {};
     data.sections = results[0].sections;
